8b3cb8fa7c11bb8c77f5bb4bc7f9df90aaa391de
PaulSoumyajit <123751553+PaulSoumyajit@users.noreply.github.com>
2024-05-21 19:41:19 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,45 @@
+if (window.performance.navigation.type === 1) {
+    rollDice();
+  }
+
+
+
+
+
+  function rollDice() {
+
+var randomNumber1 = Math.floor(Math.random() * 6) + 1 ;
+
+var randomDiceImage = "dice" + randomNumber1 + ".png";
+
+var randomImageSource = "images/" + randomDiceImage;
+
+var image1 = document.querySelectorAll("img") [0];
+
+image1.setAttribute("src", randomImageSource);
+
+
+
+var randomNumber2 = Math.floor(Math.random() * 6) + 1 ;
+
+var randomDiceImage1 = "dice" + randomNumber2 + ".png";
+
+var randomImageSource1 = "images/" + randomDiceImage1;
+
+var image2 = document.querySelectorAll("img") [1];
+
+image2.setAttribute("src", randomImageSource1);
+
+
+if ( randomNumber1 > randomNumber2 ) {
+    document.querySelector("h2").innerHTML = "🚩 Player 1 Wins"
+}
+
+else if ( randomNumber2 > randomNumber1 ) {
+    document.querySelector("h2").innerHTML = "Player 2 Wins 🚩"
+}
+
+else {
+    document.querySelector("h2").innerHTML = "Draw"
+}
+  }
